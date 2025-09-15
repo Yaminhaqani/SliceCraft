39fd3757996d8de86c54578f0fafb78a7ab88bb5
Yaminhaqani <yaminhaqani@gmail.com>
2025-09-15 17:20:36 +0530

    Initial commit

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (): Promise<void> => {
+  const URI= process.env.MONGO_AURI;
+
+  if (!URI) {
+    throw new Error("URI environment variable is not defined");
+  }
+
+  try {
+    const conn = await mongoose.connect(URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error; // when you do await connectDB() without this, it will NOT reject — it will resolve successfully, even though the DB failed to connect!
+  }
+};
